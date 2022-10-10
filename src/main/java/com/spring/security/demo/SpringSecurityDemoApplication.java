16cf815412ab0a714a9c27abce06d5a9acdfb51d
bed-com-39-19 <bed-com-39-19@unima.ac.mw>
2022-10-10 06:31:45 +0000

    Modified some files

--- src/main/java/com/spring/security/demo/SpringSecurityDemoApplication.java
+++ src/main/java/com/spring/security/demo/SpringSecurityDemoApplication.java
@@ -6,10 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringSecurityDemoApplication {
 
-//	@Bean
-//	public Docket docket(){
-//		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("pastPaperStores")).build();
-//	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityDemoApplication.class, args);
 	}
